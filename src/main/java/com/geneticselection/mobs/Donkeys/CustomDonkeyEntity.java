4bd9babe7e734b7218c6547e8dc14cd11b4b1c94
mabasm <mabasm@rpi.edu>
2024-12-02 12:03:18 -0500

    Added donkey code to mobs folder

---
+++ src/main/java/com/geneticselection/mobs/Donkeys/CustomDonkeyEntity.java
@@ -0,0 +1,129 @@
+package com.geneticselection.mobs.Donkeys;
+
+import com.geneticselection.attributes.GlobalAttributesManager;
+import com.geneticselection.attributes.MobAttributes;
+import com.geneticselection.mobs.ModEntities;
+import com.geneticselection.utils.DescriptionRenderer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.passive.DonkeyEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+import java.util.Optional;
+
+import static com.geneticselection.genetics.ChildInheritance.*;
+
+public class CustomDonkeyEntity extends DonkeyEntity {
+    private MobAttributes mobAttributes; // Directly store MobAttributes for this entity
+    private double MaxHp;
+    private double Speed;
+    private double MaxLeather;
+
+    public CustomDonkeyEntity(EntityType<? extends DonkeyEntity> entityType, World world) {
+        super(entityType, world);
+
+        // Initialize mob attributes (directly within the class)
+        if (this.mobAttributes == null) {
+            MobAttributes global = GlobalAttributesManager.getAttributes(entityType);
+            double speed = global.getMovementSpeed() * (0.98 + Math.random() * 0.1);
+            double health = global.getMaxHealth() * (0.98 + Math.random() * 0.1);
+            double leather = global.getMaxLeather().orElse(0.0) * (0.98 + Math.random() * 0.1);
+            this.mobAttributes = new MobAttributes(speed, health, Optional.empty(), Optional.of(leather), Optional.empty(), Optional.empty());
+        }
+
+        // Apply attributes to the entity
+        this.MaxHp = this.mobAttributes.getMaxHealth();
+        this.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(this.MaxHp);
+        this.Speed = this.mobAttributes.getMovementSpeed();
+        this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(this.Speed);
+        this.mobAttributes.getMaxLeather().ifPresent(maxLeather -> {
+            this.MaxLeather = maxLeather;
+        });
+    }
+
+    public void setMaxLeather(double maxLeather)
+    {
+        this.MaxLeather = maxLeather;
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        // If player has an empty hand
+        if (itemStack.isEmpty()) {
+            player.playSound(SoundEvents.ENTITY_DONKEY_AMBIENT, 1.0F, 1.0F);
+
+            // Only display the stats on the server side to avoid duplication
+            if (!this.getWorld().isClient) {
+                DescriptionRenderer.setDescription(this, Text.of("Attributes\n" +
+                        "Max Hp: " + this.MaxHp + "\nMax Leather: " + this.MaxLeather));
+            }
+            return ActionResult.success(this.getWorld().isClient);
+        } else {
+            return super.interactMob(player, hand);
+        }
+    }
+
+
+    @Override
+    public void onDeath(DamageSource source) {
+        super.onDeath(source);
+
+        if (!this.getWorld().isClient) {
+            int leatherAmount = (int)(MaxLeather);
+            this.dropStack(new ItemStack(Items.LEATHER, leatherAmount));
+        }
+    }
+
+    @Override
+    public CustomDonkeyEntity createChild(ServerWorld serverWorld, PassiveEntity mate) {
+        if (!(mate instanceof CustomDonkeyEntity)) {
+            return (CustomDonkeyEntity) EntityType.DONKEY.create(serverWorld);
+        }
+
+        CustomDonkeyEntity parent1 = this;
+        CustomDonkeyEntity parent2 = (CustomDonkeyEntity) mate;
+
+        MobAttributes attr1 = parent1.mobAttributes;
+        MobAttributes attr2 = parent2.mobAttributes;
+
+        // Inherit attributes from both parents
+        MobAttributes childAttributes = inheritAttributes(attr1, attr2);
+
+        double childMaxLeather = (parent1.MaxLeather + parent2.MaxLeather) / 2;
+
+        CustomDonkeyEntity child = new CustomDonkeyEntity(ModEntities.CUSTOM_DONKEY, serverWorld);
+
+        // Set the inherited attributes directly
+        child.mobAttributes = childAttributes;
+        applyAttributes(child, childAttributes);
+
+        child.MaxLeather = childMaxLeather;
+        child.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(child.MaxHp);
+
+        influenceGlobalAttributes(child.getType());
+
+        return child;
+    }
+
+    // Getter and Setter for mobAttributes if needed
+    public MobAttributes getMobAttributes() {
+        return this.mobAttributes;
+    }
+
+    public void setMobAttributes(MobAttributes attributes) {
+        this.mobAttributes = attributes;
+    }
+}
---
+++ src/main/java/com/geneticselection/mobs/Donkeys/CustomDonkeyRenderer.java
@@ -0,0 +1,32 @@
+package com.geneticselection.mobs.Donkeys;
+
+import com.geneticselection.GeneticSelection;
+import com.geneticselection.mobs.Donkeys.CustomDonkeyEntity;
+import com.geneticselection.mobs.ModModelLayers;
+import com.geneticselection.utils.DescriptionRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.DonkeyEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+public class CustomDonkeyRenderer extends MobEntityRenderer<CustomDonkeyEntity, DonkeyEntityModel<CustomDonkeyEntity>> {
+    private static final Identifier TEXTURE = Identifier.of(GeneticSelection.MOD_ID, "textures/entity/minecraft-pig.png");
+
+    public CustomDonkeyRenderer(EntityRendererFactory.Context context) {
+        super(context, new DonkeyEntityModel<>(context.getPart(ModModelLayers.CUSTOM_DONKEY)), 0.6f);
+    }
+
+    @Override
+    public Identifier getTexture(CustomDonkeyEntity entity) {
+        return TEXTURE;
+    }
+
+    @Override
+    public void render(CustomDonkeyEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+        // Render custom description using DescriptionRenderer
+        DescriptionRenderer.renderDescription(entity, matrices, vertexConsumers, light, this.dispatcher, this.getTextRenderer(), tickDelta);
+    }
+}
