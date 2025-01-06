8af9d722c4136ab1c8d974ae3577f490f592c558
Tony <149916666+T03N@users.noreply.github.com>
2025-01-06 16:50:06 -0500

    Delete src/main/java/com/geneticselection/genetics directory

--- src/main/java/com/geneticselection/genetics/Genetics.java
+++
@@ -1,9 +0,0 @@
-package com.geneticselection.genetics;
-
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.server.world.ServerWorld;
-
-public interface Genetics {
-    AnimalEntity breed(AnimalEntity parent1, AnimalEntity parent2, ServerWorld world);
-}
