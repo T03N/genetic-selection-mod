92b059457ad05cce9c1b6af1f407f460c604c42f
Tony <149916666+T03N@users.noreply.github.com>
2025-01-06 16:50:25 -0500

    Delete src/main/java/com/geneticselection/interfaces directory

--- src/main/java/com/geneticselection/interfaces/IGeneticEntity.java
+++
@@ -1,8 +0,0 @@
-package com.geneticselection.interfaces;
-
-import com.geneticselection.attributes.MobAttributes;
-
-public interface IGeneticEntity {
-    MobAttributes getMobAttributes();
-    void setMobAttributes(MobAttributes attributes);
-}
